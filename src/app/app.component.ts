0505aa84c93f7460cce9bab4bc9a6eabdc55d8fd
caio felipe <fcaio8966@gmail.com>
2025-04-29 14:46:55 -0300

    projeto organo

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { LivrosComponent } from './componentes/livros/livros.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    CabecalhoComponent, 
+    RodapeComponent,
+    LivrosComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'organo2';
+}
---
+++ src/app/componentes/livros/livros.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livros',
+  imports: [],
+  templateUrl: './livros.component.html',
+  styleUrl: './livros.component.css'
+})
+export class LivrosComponent {
+  livro = {
+    titulo: "As ondas",
+    autoria: "Virginia Woolf"
+}
+}
