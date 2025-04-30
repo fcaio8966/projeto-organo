a9c98fe25424250407592743faae15a3d819d2f8
caio felipe <fcaio8966@gmail.com>
2025-04-30 00:42:16 -0300

    atualizacao

--- src/app/componentes/livros/livros.component.ts
+++ src/app/componentes/livros/livros.component.ts
@@ -7,9 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './livros.component.css'
 })
 export class LivrosComponent {
+  alternarfavorito(){
+    this.livro.favorito = ! this.livro.favorito
+  }
   livro = {
     titulo: "o senhor dos anéis",
     autoria: "J. R. R. Tolkien",
+    favorito: false,
     imagem:"https://m.media-amazon.com/images/I/71ZLavBjpRL._SY425_.jpg"
 }
 }
